af14f5e7807d601b0fb5e97000c556b59ffc5882
Trevor Lang <trevorsaynever@gmail.com>
2018-06-29 22:20:11 -0600

    Adding site config

---
+++ data/SiteConfig.js
@@ -0,0 +1,43 @@
+module.exports = {
+  blogPostDir: "blog", // The name of directory that contains your posts.
+  siteTitle: "Trevor Lang: Front End Developer & Designer", // Site title.
+  siteTitleAlt: "Trevor Lang: Front End Developer & Designer", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "https://trevorlang.io", // Domain of your website without pathPrefix.
+  pathPrefix: "trevorlang", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: 'Hey There, I\'m Trevor Lang. I do Front End Development, UI/UX, and Branding based out of Denver, Colorado. Learn more about me, check out some of my recent work, or just say "Hey!"', // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
+  googleTagManagerID: "GTM-M9JT93Q", // GTM Container ID
+  googleTagManagerPreviewQA: "&gtm_auth=2CMHIw8gMegcvWXJh9eh8g&gtm_preview=env-10&gtm_cookies_win=x",
+  googleTagManagerPreviewDevelopment: "&gtm_auth=TSL1YixK1E9J_v8r3HTVQg&gtm_preview=env-8&gtm_cookies_win=x",
+  disqusShortname: "", // Disqus shortname.
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  userName: "Trevor Lang", // Username to display in the author segment.
+  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "", // User location to display in the author segment.
+  userAvatar: "", // User avatar to display in the author segment.
+  userDescription:
+    "", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/Vagr9K/gatsby-advanced-starter",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/Vagr9K",
+      iconClassName: "fa fa-twitter"
+    },
+    {
+      label: "Email",
+      url: "mailto:vagr9k@gmail.com",
+      iconClassName: "fa fa-envelope"
+    }
+  ],
+  copyright: "Copyright © 2018. Trevor Lang", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+};
